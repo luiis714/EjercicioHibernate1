b7f33091b662d3954fc2d5b04cbd84ba4c68d190
Luis <luis.mangas15@gmail.com>
2021-04-20 19:52:33 +0200

    Creado el proyecto MAVEN y añadidas las dependencias (log4j, Hibernate core, MySQL, javassist).

---
+++ src/main/java/EjercicioHibernate1_LuisM/EjercicioHibernate1_LuisM/App.java
@@ -0,0 +1,13 @@
+package EjercicioHibernate1_LuisM.EjercicioHibernate1_LuisM;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
