dee691c8ea6c33f13b5791f933add432fe3b6415
Luis <luis.mangas15@gmail.com>
2021-04-21 17:22:16 +0200

    Editadas las clases Departamento.java y Empleado.java como clases Bean.

--- src/main/java/EjercicioHibernate1_LuisM/EjercicioHibernate1_LuisM/datamodel/entities/Departamento.java
+++ src/main/java/EjercicioHibernate1_LuisM/EjercicioHibernate1_LuisM/datamodel/entities/Departamento.java
@@ -3,5 +3,48 @@ package EjercicioHibernate1_LuisM.EjercicioHibernate1_LuisM.datamodel.entities;
 import java.io.Serializable;
 
 public class Departamento implements Serializable{
+	
+	private int codigoDepartamento;
+	private String nombreDepartamento;
+	private int codResponsable;
+	
+	public Departamento() {
+		
+	}
+	
+	public Departamento(int codigoDepartamento, String nombreDepartamento, int codResponsable) {
+		this.codigoDepartamento = codigoDepartamento;
+		this.nombreDepartamento = nombreDepartamento;
+		this.codResponsable = codResponsable;
+	}
 
+	public int getCodigoDepartamento() {
+		return codigoDepartamento;
+	}
+
+	public void setCodigoDepartamento(int codigoDepartamento) {
+		this.codigoDepartamento = codigoDepartamento;
+	}
+
+	public String getNombreDepartamento() {
+		return nombreDepartamento;
+	}
+
+	public void setNombreDepartamento(String nombreDepartamento) {
+		this.nombreDepartamento = nombreDepartamento;
+	}
+
+	public int getCodResponsable() {
+		return codResponsable;
+	}
+
+	public void setCodResponsable(int codResponsable) {
+		this.codResponsable = codResponsable;
+	}
+	
+	@Override
+	public String toString() {
+		return "Departamento [codigoDepartamento=" + codigoDepartamento + ", nombreDepartamento=" + nombreDepartamento
+				+ ", codResponsable=" + codResponsable + "]";
+	}
 }
--- src/main/java/EjercicioHibernate1_LuisM/EjercicioHibernate1_LuisM/datamodel/entities/Empleado.java
+++ src/main/java/EjercicioHibernate1_LuisM/EjercicioHibernate1_LuisM/datamodel/entities/Empleado.java
@@ -3,5 +3,120 @@ package EjercicioHibernate1_LuisM.EjercicioHibernate1_LuisM.datamodel.entities;
 import java.io.Serializable;
 
 public class Empleado implements Serializable{
+	
+	private int codigoEmpleado;
+	private String nombreEmpleado;
+	private String apellido1;
+	private String apellido2;
+	private String lugarNacimiento;
+	private char fechaNacimiento;
+	private String direccion;
+	private String telefono;
+	private String puesto;
+	private int codDepartamento;
+	
+	public Empleado() {
+		
+	}
+	
+	public Empleado(int codigoEmpleado, String nombreEmpleado, String apellido1, String apellido2, String lugarNacimiento, char fechaNacimiento, String direccion, String telefono, String puesto, int codDepartamento) {
+		this.codigoEmpleado = codigoEmpleado;
+		this.nombreEmpleado = nombreEmpleado;
+		this.apellido1 = apellido1;
+		this.apellido2 = apellido2;
+		this.lugarNacimiento = lugarNacimiento;
+		this.fechaNacimiento = fechaNacimiento;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.puesto = puesto;
+		this.codDepartamento = codDepartamento;
+	}
 
+	public int getCodigoEmpleado() {
+		return codigoEmpleado;
+	}
+
+	public void setCodigoEmpleado(int codigoEmpleado) {
+		this.codigoEmpleado = codigoEmpleado;
+	}
+
+	public String getNombreEmpleado() {
+		return nombreEmpleado;
+	}
+
+	public void setNombreEmpleado(String nombreEmpleado) {
+		this.nombreEmpleado = nombreEmpleado;
+	}
+
+	public String getApellido1() {
+		return apellido1;
+	}
+
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+
+	public String getApellido2() {
+		return apellido2;
+	}
+
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+
+	public String getLugarNacimiento() {
+		return lugarNacimiento;
+	}
+
+	public void setLugarNacimiento(String lugarNacimiento) {
+		this.lugarNacimiento = lugarNacimiento;
+	}
+
+	public char getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(char fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getPuesto() {
+		return puesto;
+	}
+
+	public void setPuesto(String puesto) {
+		this.puesto = puesto;
+	}
+
+	public int getCodDepartamento() {
+		return codDepartamento;
+	}
+
+	public void setCodDepartamento(int codDepartamento) {
+		this.codDepartamento = codDepartamento;
+	}
+
+	@Override
+	public String toString() {
+		return "Empleado [codigoEmpleado=" + codigoEmpleado + ", nombreEmpleado=" + nombreEmpleado + ", apellido1="
+				+ apellido1 + ", apellido2=" + apellido2 + ", lugarNacimiento=" + lugarNacimiento + ", fechaNacimiento="
+				+ fechaNacimiento + ", direccion=" + direccion + ", telefono=" + telefono + ", puesto=" + puesto
+				+ ", codDepartamento=" + codDepartamento + "]";
+	}
 }
